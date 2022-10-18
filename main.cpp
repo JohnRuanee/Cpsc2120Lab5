dfcc52961dbd895ee742f7599c047e34a62a9107
JohnRuanee <65741799+JohnRuanee@users.noreply.github.com>
2022-10-18 19:53:16 -0400

    comments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /*
- * Name:
- * Date Submitted:
- * Lab Section:
- * Assignment Name:
+ * Name: John Ruane
+ * Date Submitted: 10/18
+ * Lab Section: 001
+ * Assignment Name: Lab 5
  */
  
  #include "stringset.h"
@@ -76,28 +76,33 @@
      } while (choice[0] != 'Q' && choice[0] != 'q');
  }
  
-  void loadStringset(Stringset& words, string filename)
- {
+//loads file
+void loadStringset(Stringset& words, string filename){
+    //opens file
     ifstream infile(filename);
     string word;
+    //gets each word and adds it to the table
     while(getline(infile, word)){
         words.insert(word);
     }
  }
  
- vector<string> spellcheck(const Stringset& words, string word)
- {
+vector<string> spellcheck(const Stringset& words, string word){
+    //vars
     vector<string> temp;
     string test;
 
+    //loops through changing one letter of word at a time till it goes through whole alphabet on each letter
     for(int i = 0; i < 26; i++){
         for(int n = 0; n < word.size(); n++){
             test = word;
             test[n] = char(i+97);
             
+            //if one of these changed words is in table and if its not the original word add it to temp
             if(words.find(test) && test != word)
                 temp.push_back(test);
         }
     }
+    //returns temp
     return temp;
  }
--- stringset.cpp
+++ stringset.cpp
@@ -1,8 +1,8 @@
 /*
- * Name:
- * Date Submitted:
- * Lab Section:
- * Assignment Name:
+ * Name: John Ruane
+ * Date Submitted: 10/18
+ * Lab Section: 001
+ * Assignment Name: Lab 5
  */
 
 #include "stringset.h"
@@ -27,49 +27,72 @@ int Stringset::getSize() const{
     return size;
 }
 
+//inserts word into table
 void Stringset::insert(string word){
+    //if word is alread in the table exit
     if(find(word))
         return;
 
+    //opens a hash
     hash<string> hash;
+
+    //if size is too small to add another element
     if(num_elems == size){
+        //double the size
         size = size * 2;
+        //temp vector of size
         vector<list<string>> temp(size);
 
+        //go through old vector and reassign it to new vector
         for(int i = 0; i < size / 2; i++){
             for(int n = table[i].size(); n > 0; n--){
+                //sets a temp string of front of table
                 string tempS = table[i].front();
+                //set h to hash for an insert location
                 int h = hash(tempS) % size;
                 
+                //pushes temps to front of temp then removes temp s from table
                 temp[h].push_front(tempS);
                 table[i].remove(tempS);
             }
         }
+        //set table equal to temp
         table = temp;
     }
+    //increases the number of elements
     num_elems++;
 
+    //sets insert location to hash % size
     int insert_location = hash(word) % size;
 
+    //pushes word at front of table at insert location
     table[insert_location].push_front(word);
 }
 
+//if word is in stringset returns true
 bool Stringset::find(string word) const{
+    //opens hash
     hash<string> hash;
+    //sets search location to hash of word % size
     int h = hash(word) % size;
 
+    //goes through table at h and gets each word and checks if it equals word
     for(string str : table[h]){
+        //if so returns true
         if(str == word){
             return true;
         }
     }
-    
+    //else return false
     return false;
 }
 
+//removes word from table
 void Stringset::remove(string word){
+    //goes through each part of table and removes word
     for(int i = 0; i < size; i++)
         table[i].remove(word);
+    //decriments num elements
     num_elems--;
 }
 
